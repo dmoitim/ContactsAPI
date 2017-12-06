b73cff161bc18acd48a8817d155ed41603adbd68
Devair Luiz Moitim <dmoitim@gmail.com>
2017-12-05 22:35:35 -0200

    Contacts API

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  acts_as_token_authenticatable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :contacts, dependent: :destroy
+end
